75013a6c4d88f01f88c47eb78c5e7fb168b8afa9
luigi617 <luigiliu617@gmail.com>
2024-10-29 13:16:14 -0400

    watch session

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import datetime
 from flask import Flask, jsonify, request
 from database.rds_database import rds_database
-from models import Recommendation_Service_Model
+from models import Recommendation_Service_Model, WatchSession
 from dataclasses import asdict
 from util import * 
 import os
@@ -38,6 +39,88 @@ def recommend_resources(keyword):
         return jsonify({"message": "No matching resources found."})
     resource_ids = [entry['resource_id'] for entry in result]
     return jsonify(resource_ids)
+
+@app.route("/start_watch", methods=['POST'])
+def user_start_watch():
+    data = request.get_json()
+    user_id = data.get('user_id')
+    stream_id = data.get('stream_id')
+
+    if not user_id or not stream_id:
+        return jsonify({'error': 'user_id and stream_id are required'}), 400
+
+    existing_session = cur_database.query_data(
+        "watch_session", 
+        ["user_id", "stream_id"], 
+        {"user_id": user_id, "stream_id": stream_id}
+    )
+
+    if len(existing_session) > 0:
+        cur_database.update_data(
+            "watch_session",
+            {
+                "watch_start_time": datetime.datetime.now(datetime.timezone.utc),
+                "watch_end_time": None
+            },
+            {
+                "user_id": user_id,
+                "stream_id": stream_id
+            }
+        )
+        message = "Session updated successfully."
+    else:
+        new_session = {
+            "user_id": user_id,
+            "stream_id": stream_id,
+            "watch_start_time": datetime.datetime.now(datetime.timezone.utc),
+            "watch_end_time": None,
+            "duration": 0
+        }
+        cur_database.bulk_insert_data("watch_session", [new_session])
+        message = "Session inserted successfully."
+
+    return jsonify({"message": message}), 200
+
+
+
+@app.route('/end_watch', methods=['POST'])
+def end_watch():
+    data = request.get_json()
+    user_id = data.get('user_id')
+    stream_id = data.get('stream_id')
+
+    if not user_id or not stream_id:
+        return jsonify({'error': 'user_id and stream_id are required'}), 400
+
+    session_data = cur_database.query_data(
+        "watch_session", 
+        ["user_id", "stream_id", "watch_start_time"], 
+        {"user_id": user_id, "stream_id": stream_id}
+    )
+
+    if len(session_data) == 0:
+        return jsonify({'message': "session does not exist"}), 400
+
+    # Retrieve watch_start_time and calculate duration
+    watch_start_time = session_data[0]['watch_start_time']
+    watch_end_time = datetime.datetime.now(datetime.timezone.utc)
+    duration = (watch_end_time - watch_start_time).total_seconds()
+
+    # Update the session with end time and duration
+    cur_database.update_data(
+        "watch_session",
+        {
+            "watch_end_time": watch_end_time,
+            "duration": duration     
+        },
+        {
+            "user_id": user_id,
+            "stream_id": stream_id
+        }
+    )
+
+    return jsonify({'message': 'Watch session ended', 'duration': duration}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
--- models/Recommendation_Service_Model.py
+++ models/Recommendation_Service_Model.py
@@ -1,7 +1,17 @@
 from dataclasses import dataclass
+import datetime
 
 @dataclass
 class Label:
     resource_id: int
     label: str
+
+
+@dataclass
+class WatchSession:
+    user_id: int
+    stream_id: int
+    watch_start_time: datetime
+    watch_end_time: datetime
+    duration: float
 
